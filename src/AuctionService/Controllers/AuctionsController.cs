9b796413df6aa60ad4003aed2359a47544135068
Zou Zhefeng <archer.zou84@gmail.com>
2025-07-08 12:52:16 +1200

    setup project

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,98 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuctionsController(AuctionDbContext context, IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auction = await context.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Make)
+            .ToListAsync();
+
+        return mapper.Map<List<AuctionDto>>(auction);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null) return NotFound();
+
+        return mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = mapper.Map<Auction>(auctionDto);
+        // TODO: add current user as seller
+        auction.Seller = "test";
+        context.Auctions.Add(auction);
+        
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Failed to create auction");
+        
+        return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, mapper.Map<AuctionDto>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<AuctionDto>> UpdateAuction(Guid id, [FromBody] UpdateAuctionDto auctionDto)
+    {
+        var auction = await context.Auctions.Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null) return NotFound();
+
+        // TODO: check seller == username
+        auction.Item.Make = auctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = auctionDto.Model ?? auction.Item.Model;
+        auction.Item.Year = auctionDto.Year;
+        auction.Item.Color = auctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = auctionDto.Mileage;
+
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Failed to update auction");
+        }
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await context.Auctions.FindAsync(id);
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        // if (auction.Seller != User.Identity?.Name) return Forbid();
+        context.Auctions.Remove(auction);
+
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Failed to delete auction");
+        }
+
+        return Ok();
+    }
+
+}
