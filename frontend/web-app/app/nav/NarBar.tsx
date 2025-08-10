f832f1496d2f8b476ac3dfbcb36a0a3dc4159441
Zou Zhefeng <archer.zou84@gmail.com>
2025-08-10 23:54:44 +1200

    client side identity with authjs

--- frontend/web-app/app/nav/NarBar.tsx
+++ frontend/web-app/app/nav/NarBar.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+'use client';
+
 import Search from "@/app/nav/Search";
 import Logo from "@/app/nav/Logo";
 
+import { useSession } from "next-auth/react";
+import UserActions from "@/app/nav/UserActions";
+import LoginButton from "@/app/nav/LoginButton";
+
 const NarBar = () => {
+    const session = useSession();
     return (
-        <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
+        <header className="sticky top-0 z-50 flex justify-between
+            bg-white p-5 items-center text-gray-800 shadow-md">
             <Logo />
             <Search />
-            <div>Login</div>
+            {session.data?.user ? (
+                <UserActions user={session.data.user} />
+            ) : (
+                <LoginButton />
+            )}
         </header>
     );
 };
