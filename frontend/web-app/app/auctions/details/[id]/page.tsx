189f4bd3ac1efc942b9d7d03d51abe20a4750e03
Zou Zhefeng <archer.zou84@gmail.com>
2025-09-27 01:12:50 +1200

    finish the auction CRUD actions and pages

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,12 +1,51 @@
+import {getDetailedViewData} from "@/app/actions/auctionActions";
+import Heading from "@/app/components/Heading";
+import {getCurrentUser} from "@/app/actions/authActions";
+import CountdownTimer from "@/app/auctions/CountdownTimer";
+import EditButton from "@/app/auctions/EditButton";
+import DeleteButton from "@/app/auctions/DeleteButton";
+import CarImage from "@/app/auctions/CarImage";
+import DetailedSpecs from "@/app/auctions/DetailedSpecs";
 
 const Details = async ({ params}: {params: Promise<{id: string}>}) => {
     const { id } = await params;
+    const data = await getDetailedViewData(id);
+    const user = await getCurrentUser();
 
     return (
-        <div>
-            Details for: {id}
-        </div>
-    );
+        <>
+            <div className="flex justify-between">
+                <div className="flex items-center gap-3">
+                    <Heading title={`${data.make} ${data.model}`} />
+                    {user?.username === data.seller && (
+                        <>
+                            <EditButton id={data.id} />
+                            <DeleteButton id={data.id} />
+                        </>
+
+                    )}
+                </div>
+
+                <div className="flex gap-3">
+                    <h3 className="text-2xl font-semibold">Time remaining:</h3>
+                    <CountdownTimer auctionEnd={data.auctionEnd} />
+                </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-6 mt-3">
+                <div className="relative w-full bg-gray-200 aspect-[16/10]
+                    rounded-lg overflow-hidden">
+                    <CarImage imageUrl={data.imageUrl} />
+                </div>
+                {/*<BidList user={user} auction={data} />*/}
+            </div>
+
+            <div className="mt-3 grid grid-cols-1 rounded-lg">
+                <DetailedSpecs auction={data} />
+            </div>
+        </>
+
+    )
 };
 
 export default Details;
--- frontend/web-app/app/auctions/update/[id]/page.tsx
+++ frontend/web-app/app/auctions/update/[id]/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { getDetailedViewData } from "@/app/actions/auctionActions";
+import Heading from "@/app/components/Heading";
+import AuctionForm from "../../AuctionForm";
 
-const Update = () => {
+
+const Update = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
+    const data = await getDetailedViewData(id);
+
     return (
-        <div>
-            Update Page
+        <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+            <Heading title="Update your auction" subtitle="Please update the details of your
+                car (only these auction properties can be updated)" />
+            <AuctionForm auction={data} />
         </div>
-    );
+    )
 };
 
 export default Update;
